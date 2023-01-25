5b2e929f571bca06541ffa251c9a3f2eb60abe29
yousuke <sadadarame@gmail.com>
2023-01-25 16:05:16 +0900

    Initial Commit

---
+++ dustCalrendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dustCalrendar
+//
+//  Created by Yosuke Yoshida on 2023/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dustCalrendar/dustCalrendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  dustCalrendarApp.swift
+//  dustCalrendar
+//
+//  Created by Yosuke Yoshida on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct dustCalrendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
